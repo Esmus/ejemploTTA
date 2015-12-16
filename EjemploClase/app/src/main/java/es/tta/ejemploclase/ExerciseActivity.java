b82181f64ca4ae5f9b38f1039a21dcca4ba9050c
user.email <mjcava001@ikasle.ehu.eus>
2015-12-16 21:36:41 +0100

    Añadidos Toast alerta de funciones no implementas

--- EjemploClase/app/src/main/java/es/tta/ejemploclase/ExerciseActivity.java
+++ EjemploClase/app/src/main/java/es/tta/ejemploclase/ExerciseActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 public class ExerciseActivity extends AppCompatActivity {
 
@@ -15,7 +16,38 @@ public class ExerciseActivity extends AppCompatActivity {
         setContentView(R.layout.activity_exercise);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        
+    }
 
+
+    public void subirFichero(View view){
+
+
+        Toast toast = Toast.makeText(this, "No implementada la accion de subirFichero", Toast.LENGTH_SHORT);
+        toast.show();
+
+    }
+
+    public void sacarFoto(View view){
+
+        Toast toast = Toast.makeText(this, "No implementada la accion de subirFoto", Toast.LENGTH_SHORT);
+        toast.show();
+
+
+    }
+
+    public void recordAudio(View view){
+
+        Toast toast = Toast.makeText(this, "No implementada la accion de recordAudio", Toast.LENGTH_SHORT);
+        toast.show();
+
+
+    }
+
+    public void recordVideo(View view){
+
+        Toast toast = Toast.makeText(this, "No implementada la accion de recordVideo", Toast.LENGTH_SHORT);
+        toast.show();
 
     }
 
