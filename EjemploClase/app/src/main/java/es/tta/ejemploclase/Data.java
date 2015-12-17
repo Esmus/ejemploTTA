f3292b99cbaa31d5308efb9dd08320055e5c2352
user.email <mjcava001@ikasle.ehu.eus>
2015-12-17 12:31:11 +0100

    Añadidas clase data y test 

---
+++ EjemploClase/app/src/main/java/es/tta/ejemploclase/Data.java
@@ -0,0 +1,12 @@
+package es.tta.ejemploclase;
+
+/**
+ * Created by Miren on 17/12/2015.
+ */
+public class Data {
+    public Test getTest(){
+
+        return new Test();
+
+    }
+}
--- EjemploClase/app/src/main/java/es/tta/ejemploclase/TestActivity.java
+++ EjemploClase/app/src/main/java/es/tta/ejemploclase/TestActivity.java
@@ -2,19 +2,19 @@ package es.tta.ejemploclase;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.webkit.WebView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Button;
 import android.widget.Toast;
-import android.graphics.Color;
+
 public class TestActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private int correct=0;
+    private String advise;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,79 +23,85 @@ public class TestActivity extends AppCompatActivity implements View.OnClickListe
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        final Button sendButton = (Button) findViewById(R.id.test_button_send);
-        final Button helpButton = (Button) findViewById(R.id.test_button_ayuda);
 
-        String preguntaTest="¿Cual de las siguientes opciones NO se indica en el fichero de manifiesto de la app?";
+        Data data= new Data();
+        Test test= data.getTest();
+
+
+
         TextView textWording=(TextView)findViewById(R.id.test_wording);
-        textWording.setText(preguntaTest);
+        textWording.setText(test.getWording());
 
-        //rellenar el radioChoice
-
-        String[] contenidoTest = {
-                "Versión de la aplicación",
-                "Listado de componentes de la aplicación",
-                "Opciones del menú de ajustes",
-                "Nivel mínimo de la API Android requerida",
-                "Nombre del paquete java de la aplicación"
-        };
 
         int i=0;
 
-        final RadioGroup group= (RadioGroup)findViewById(R.id.test_radioGroup);
+        RadioGroup group= (RadioGroup)findViewById(R.id.test_radioGroup);
 
-
-        for(i=0;i<contenidoTest.length;i++){
+        for(Test.Choice choice : test.getChoices()){
 
             RadioButton radio=new RadioButton(this);
-            radio.setText(contenidoTest[i]);
+            radio.setText(choice.getWording());
             radio.setOnClickListener(this);//hacer visible el botton enviar
             group.addView(radio);
-
+            if(choice.isCorrecta()){
+                correct=i;
+            }
+            i++;
 
            }
 
 
-        sendButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sendButton.setVisibility(View.GONE); //La view desaparece  y su espacio queda libre
+        advise="consejo......";
+
+    }
+
+    @Override
+    public void onClick(View view){
+
+        findViewById(R.id.test_button_send).setVisibility(View.VISIBLE);
 
 
-                int correct=2;
-                Boolean advise=true;
+    }
 
-                int choices=group.getChildCount();
-                for(int i=0;i<choices;i++){
-                    group.getChildAt(i).setEnabled(false);
-
-                }
-
-                int selected = group.getCheckedRadioButtonId()-1;
-                group.getChildAt(correct).setBackgroundColor(Color.GREEN);
+    public void send(View view){
 
 
-                if (selected != correct) {
-                    group.getChildAt(selected).setBackgroundColor(Color.RED);
-                    Toast.makeText(getApplicationContext(), "Has fallado!", Toast.LENGTH_SHORT).show();
-
-                    if(advise!=null){
-                        helpButton.setVisibility(View.VISIBLE);
+        RadioGroup group= (RadioGroup)findViewById(R.id.test_radioGroup);
+        findViewById(R.id.test_button_send).setVisibility(View.GONE);
 
 
-                    }
-                } else {
-                    Toast.makeText(getApplicationContext(), "Has acertado", Toast.LENGTH_SHORT).show();
-                }
+        int choices=group.getChildCount();
+        for(int i=0;i<choices;i++){
+            group.getChildAt(i).setEnabled(false);//off el grupo
+
+        }
+
+        int selectedID = group.getCheckedRadioButtonId();
+        group.getChildAt(correct).setBackgroundColor(Color.GREEN);
+        View radioButton= group.findViewById( selectedID);
+        int selected=group.indexOfChild(radioButton);
+
+
+        if (selected != correct) {
+            group.getChildAt(selected).setBackgroundColor(Color.RED);
+            Toast.makeText(getApplicationContext(), "Has fallado!", Toast.LENGTH_SHORT).show();
+
+            if(advise!=null && !advise.isEmpty() ){
+
+               findViewById(R.id.test_button_ayuda).setVisibility(View.VISIBLE);
 
 
             }
-        });
+        } else {
+            Toast.makeText(getApplicationContext(), "Has acertado", Toast.LENGTH_SHORT).show();
+        }
 
-        helpButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
+
+    }
+
+
+    public void ayuda(View view){
 
                 TextView helpText = (TextView) findViewById(R.id.test_texto_ayuda);
                 helpText.setText(R.string.test_texto_help);
@@ -108,18 +114,6 @@ public class TestActivity extends AppCompatActivity implements View.OnClickListe
                 layout.addView(web);
                 */
 
-            }
-        });
 
     }
-
-    @Override
-    public void onClick(View view){
-
-        findViewById(R.id.test_button_send).setVisibility(View.VISIBLE);
-
-    }
-
-
-
 }
